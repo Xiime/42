67ddb81ec30661f82f88c27e8d921488610f57b7
Maxime TRUDEL <mtrudel@e2r7p3.42.fr>
2017-01-24 14:28:31 +0100

    get next line marche mais uniquement quand il y a des \n

--- GetNextLine/get_next_line.c
+++ GetNextLine/get_next_line.c
@@ -6,33 +6,72 @@
 /*   By: mtrudel <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/08 17:24:31 by mtrudel           #+#    #+#             */
-/*   Updated: 2016/12/14 19:49:51 by mtrudel          ###   ########.fr       */
+/*   Updated: 2017/01/24 14:21:40 by mtrudel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		get_next_line(const int fd, char **line)
+int		ft_go_to_lbsn(char *str)
 {
-	char	buf[BUFFER + 1];
-	int		ret;
-	char	*tmp;
 	size_t	i;
 
-	tmp = "";
 	i = 0;
-	while ((ret = read(fd, buf, BUFFER)) != 0)
+	while (str[i] != '\0')
+	{
+		if (str[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (0);
+}
+
+int		get_next_line(const int fd, char **line)
+{
+	char			buf[BUFF_SIZE + 1];
+	int				ret;
+	static char		*resu;
+	size_t			i;
+	char			*tmp;
+
+	if (!resu)
+		resu = ft_strdup("");
+	i = 0;
+	if (ft_strchr(resu, '\n') != NULL && ft_strlen(resu) > 1)
+	{
+		*line = ft_strnew(ft_go_to_lbsn(resu));
+		*line = ft_strncpy(*line, resu, ft_go_to_lbsn(resu));
+		if (((int)ft_strlen(resu) - 1) >= ft_go_to_lbsn(resu))
+		{
+			tmp = ft_strdup(ft_strchr(resu, '\n') + 1);
+			resu = ft_strdup(tmp);
+			ft_strdel(&tmp);
+		}
+		return (1);
+	}
+	while ((ret = read(fd, buf, BUFF_SIZE)))
 	{
 		buf[ret] = '\0';
-		tmp = ft_strjoin(tmp, buf);
-		if (ft_strchr(tmp, '\n') != NULL)
+		resu = ft_strjoin(resu, buf);
+		if (ft_strchr(buf, '\n') != NULL)
 		{
-			while (tmp[i] != '\n')
-				i++;
-			*line = ft_strnew(i);
-			ft_strncpy(*line, tmp, i);
-			return (1);
+				*line = ft_strnew(ft_go_to_lbsn(resu));
+				*line = ft_strncpy(*line, resu, ft_go_to_lbsn(resu));
+				if (((int)ft_strlen(resu) - 1) >= ft_go_to_lbsn(resu))
+				{
+					tmp = ft_strdup(ft_strchr(resu, '\n') + 1);
+					resu = ft_strdup(tmp);
+					ft_strdel(&tmp);
+				}
+				return (1);
 		}
+	}
+	if (ret == 0 && strchr(resu, '\n') == NULL && resu != NULL)
+	{
+		*line = ft_strnew(ft_strlen(resu));
+		*line = ft_strncpy(*line, resu, ft_strlen(resu));
+		ft_strdel(&resu);
+		return (1);
 	}
 	return (0);
 }
